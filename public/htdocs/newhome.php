3acf389055d7bd25e2b3859ebf059e72f80907bf
Matthew Knight <matthew@thinkplaymake.co>
2012-07-02 15:35:11 +0100

    Copy and layout tweaks ready for live.
    Created a PHP version of the template ready for integration with
    wordpress.
    This template is still pretty messy, but will tidy it up now we've
    launched, whilst adding in a responsive version for mobile users.

---
+++ public/htdocs/newhome.php
@@ -0,0 +1,459 @@
+<?php
+	/* Template Name: New Home */
+
+
+?><!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Child's i Foundation - helping abandoned children</title>
+	<meta name="description" content="Reuniting abandoned children with families, instead of sending them to orphanages">
+	
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="/stylesheets/base.css">
+	<link rel="stylesheet" href="/stylesheets/style.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<!--
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	-->
+
+</head>
+<body>
+
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+	
+	
+	
+	<div class="container" id="section1">
+	
+	
+		<div class="row">
+			<div class="col">
+				<div id="jumpdown" class="row">
+					<div class="copy col">Already know how you're helping abandoned children? <a href="#homepage">Skip to the latest updates</a></div>
+					<!-- <div class="image col"><a href="#"><img src="/images/cif/icon_down.png" alt=""></a></div> -->
+				</div>
+			</div>
+			<div class="col">
+				<div id="jumpdonate" class="row">
+					<div class="copy col"><a href="http://childsifoundation.org/give/">Donate Now</a></div>
+					<div class="image col"><a href="http://childsifoundation.org/give/"><img src="/images/cif/icon_love.png" alt=""></a></div>
+				</div>
+			</div>
+			
+		</div>
+
+		<div>
+			<div class="row">
+			  <div class="col">
+				<div id="headerlogo"><img src="/images/cif/childsi.png" alt="childs i foundation" /></div>
+				<h2><span class="orange">Polly</span> is two.</h2>
+			  </div>
+			  <div class="col">
+				<img src="/images/cif/polly.png" alt="image of polly" />
+			  </div>
+			</div>
+		</div>
+
+	</div><!-- container -->
+	
+	<div style="" class="hr_tear"></div>
+	
+	<div class="container" id="section2">
+	
+		<div class="row" id="r1">
+			<div class="col">
+				<h2>She's been<br/> abandoned but <br/><div class="orange">Polly is not<br/>an orphan</div></h2>
+			</div>
+			<div class="col imageblock">
+				<div class="image"><img src="/images/cif/section1-image1.png" alt="" /></div>
+				<div class="copy">
+					<span class="orange">Every week, children like Polly are abandoned in Uganda,</span> left to fend for themselves in ditches and empty houses.
+				</div>
+			</div>
+		</div>
+		
+		<div class="row" id="r2">
+			<div class="image"><img src="/images/cif/section1-image2.png" alt="" /></div>
+			<div class="copy">
+				In fact, <span class="orange">80% of abandoned children in orphanages have family</span>. We believe all children, like Polly, should grow up in a loving family.
+			</div>
+		</div>
+		
+		<div class="row" id="r3">
+			<h2>
+				Childs i Foundation<br/>
+				<span class="orange">reunites abandoned children with their families,</span><br/> 
+				instead of sending them to orphanages...
+			</h2>
+		</div>
+		
+			
+	</div><!-- container -->
+	
+	<div class="hr_tear"></div>
+
+	
+	<div class="container" id="section3" >
+	
+		<div class="row">
+			<div class="col">
+			
+				<div class="image">
+					<img src="/images/cif/frame2.png" alt="image" />
+				</div>
+				<div class="copy row">
+					<div class="col">
+						<p>After spending a few months in our transitional home regaining her strength, we located Polly's family. They welcomed her into their home with open arms.</p>
+						<p>If we hadn't found Polly's family, we would have found her a new family in Uganda.</p>
+					</div>
+					<div class="col">
+						
+						<p>We find foster and adoptive families in Uganda for children life Polly where they can grow up loved and cherished in a family.</p>
+					</div>
+					
+				</div>
+			
+			</div>
+			
+			
+			
+			
+			<div class="col" id="learnabout">
+
+				<a href="http://www.childsifoundation.org/our-projects/malaika-care/" class="ctabutton">
+					Learn about
+					<span>Transitional Care</span>
+				</a>
+				
+				<a href="http://www.childsifoundation.org/blog/2012/01/finding-families/" class="ctabutton">
+					Find out how we 
+					<span>Trace and Reunite Families</span>
+				</a>
+				
+				<a href="http://www.childsifoundation.org/our-projects/social-work/" class="ctabutton">
+					Read about
+					<span>Adoption and Fostering</span>
+				</a>
+				
+				
+			
+			</div>
+		</div>
+		
+	</div>
+	
+	<div style="position: absolute; width:100%; z-index: -1; background: none; height: 760px; background-image:url(/images/cif/gradbottom.png); background-repeat:repeat-x; margin-top: -220px;"></div>
+
+	<div class="container" id="section4">
+		
+		
+		<div class="row">
+			<h2>Our approach works<br/><span class="orange">long term</span></h2>
+		</div>
+	
+		<div class="row">
+			<div class="image"><img src="/images/cif/section4-image1.png" alt="our approach works"></div>
+		</div>
+		
+		<div class="row" id="approach">
+		
+			<div class="col">
+				<p>Instead of being raised in orphanages, <span class="orange">we aim to find families for all children</span> - over 80 and counting</p>
+				<a href="https://maps.google.com/maps/ms?vps=2&ie=UTF8&hl=en&oe=UTF8&msa=0&msid=207724907407838177202.0004bb08cb15664849dbe" class="ctabutton">placement map<span>See the Results</span></a>
+			</div>
+			<div class="col">
+				<p>We work out why they've been abandoned, and support struggling families to <span class="orange">stop it happening in the first place</span>.</p>
+				<a href="http://www.childsifoundation.org/our-projects/prevention/" class="ctabutton">learn about<span>Family Support</span></a>
+			</div>
+			<div class="col last">
+				<p><span class="orange">The Ugandan government see this success.</span> We work with them to support their policy, and replicate it across Uganda.</p>
+				<a href="http://www.childsifoundation.org/our-projects/promoting-alternative-care/" class="ctabutton">how we deliver<span>Policy and Education</span></a>
+			</div>
+			
+			
+		</div>
+	
+	
+	
+	
+	
+	</div>
+	
+	<div style="position: absolute; width:100%; z-index: -1; background: none; height: 760px; background-image:url(/images/cif/gradbottom.png); background-repeat:repeat-x; margin-top: -220px;"></div>
+
+	
+	
+	
+	<div class="container" id="section5">
+		
+		<div class="row">
+			<h2 class="one">so <span class="orange">we need</span> your</h2>
+			<h2 class="two"><span class="orange">ongoing support</span></h2>
+		</div>
+		
+		
+		<div class="row">
+			<div class="col">
+				<img src="" />
+			</div>
+			<div class="col" id="needsupport">
+				<p>We're able to bring this hope to abandoned babies and vulnerable families using the <span class="orange">time, money and love given by our family of donors, supporters and workers</span>. The need for our work goes on - and you can help it go even further.</p>
+			</div>
+		</div>
+		
+		
+		<div class="row">
+			<div class="col giveblock money">
+				<h3>Give Money</h3>
+				<p>
+					We need your regular support to keep our
+					project alive, and give hope to the thousands
+					of abandoned children in Uganda.
+					See how far a monthly donation of just &pound;5 goes.
+				</p>
+				<h2><a href="http://www.childsifoundation.org/give/" class="donatenow" style="color:#09F; font-size: 70%; text-decoration:none;">&gt;Donate Now</a></h2>
+			</div>
+		</div>
+		
+		
+		<div class="row">
+			
+			<div class="col giveblock time">
+				<h3>Give Time</h3>
+				<p>
+					We're a community of supporters who offer our time to help in dozens of different ways.
+					
+					Offer your time by hosting fundraisers,
+					getting involved in projects,
+					or even becoming a fulltime employee.
+					
+				</p>
+				<h2><a href="http://getinvolved.childsifoundation.org/" class="donatenow" style="color:#0C6; font-size: 70%; text-decoration:none;">&gt;Get Involved</a></h2>
+				
+			</div>
+			
+			
+			
+			<div class="col giveblock love">
+				<h3>Give Love</h3>
+				<p>
+					Tell your friends about what we do, write articles, come along to meetups and events.
+					The more people who understand what we do, and how we do it, the better we're able to change the lives of abandoned children for the better.
+				</p>
+				<div class="row">
+					<div class="col"><a href="http://facebook.com/groups/childsifoundation/"><img src="/images/cif/facebook.png" /></a></div>
+					<div class="col"><a href="https://twitter.com/childsi"><img src="/images/cif/twitter.png" /></a></div>
+				</div>
+			</div>
+			
+		</div>
+	
+	
+	
+		<div class="row" style="text-align: center; margin-top: 120px;">
+			<div><img src="/images/cif/childsi.png" alt="childs i foundation" /></div>
+			<h2 style="font-size: 200%">now you know what we've done,<br/>find out what we're doing, <span class="orange">right now...</span>
+		</div>
+	
+	
+	</div>
+	
+	
+	
+	<div style="position: absolute; width:100%; z-index: -1; background: none; height: 760px; background-image:url(/images/cif/gradbottom.png); background-repeat:repeat-x; margin-top: -220px;"></div>
+
+	
+	
+	<div class="container" id="homepage">
+		
+		<div class="row">
+		
+			<div class="col">
+				<div id="headerlogo"><img src="/images/cif/childsi.png" alt="childs i foundation" /></div>
+			</div>
+			<div class="col" id="nav">
+				<ul>
+					<li><a href="http://www.childsifoundation.org/our-mission/">Learn more</a></li>
+					<li><a href="http://www.childsifoundation.org/community">Get Involved</a></li>
+					<li><a href="http://www.childsifoundation.org/blog">Updates</a></li>
+					<li><a href="http://www.childsifoundation.org/give">Donate</a></li>
+				</ul>
+			</div>
+		
+		</div>
+		
+		
+		<div class="row">
+		
+			<div class="col">
+				<h3>Latest from the blog</h3>
+				
+				<div class="post">
+					<h4><a href="http://www.childsifoundation.org/blog/2012/06/woty/">Clarins Most Dynamisante Woman of the Year</a></h4>
+					<div class="postmeta">Posted by <span class="orange">Kirsty</span> on <span class="orange">June 30th, 2012</span></div>
+					<div class="featimage" style="background-image:url(http://www.childsifoundation.org/wp-content/uploads/2012/06/lucy_woman_of_the_year_2.jpg);background-size:cover;width: 600px; height: 350px;"></div>
+					<div class="excerpt">
+						<p>For the eleventh year running Clarins and YOU Magazine joined forces to find Britain's Most Dynamisante Woman of the Year. Our very own, incredibly inspirational Lucy Buck was awarded this honour and the whopping &pound;30,000 that will enable our foster care pilot. More information on this to follow. 
+						<a href="http://www.childsifoundation.org/blog/2012/06/woty/">Read More</a></p>
+					</div>
+				</div>
+			
+			
+				<div class="row" id="features">
+					<div class="col">
+						<h3>Upcoming Event</h3>
+						<h4><a href="http://getinvolved.childsifoundation.org/fundraising/events/">Fashion Show, Tunbridge Wells, UK</a></h4>
+						<p>
+							"Love Is Boutique" is a designer dress agency and vintage boutique based in the heart of Kent in a beautiful 4 storey town house. They are holding a fashion show in conjunction with Child's i Foundation.
+						</p>
+					</div>
+					<div class="col">
+						<h3>Current Opportunities</h3>
+						<h4><a href="http://getinvolved.childsifoundation.org/opportunity/fundraising-intern/">Fundraising Intern</a></h4>
+						<p>
+							Our intern will provide vital administrative support and work closely with our community and online fundraising teams, undertake prospect research, gain proposal writing experience, and assist with all aspects of day-to-day operations. 
+						</p>
+					</div>
+					
+				</div>
+			
+			
+			
+			</div>
+			<div class="col">
+			
+			
+				<div class="panel" style="margin-top: 40px;">
+					<h2><span class="orange">donate<br/>right now</span></h2>
+					<p>
+						Help us continue our work across Uganda. <span class="orange">&pound;1 a week can pay for a foster family's education and support</span>.
+					</p>
+
+					<p>
+						It's easy to setup <span class="orange">regular or one-off gifts.</span>
+					</p>
+				</div>
+				<a href="http://www.childsifoundation.org/give/" class="ctabutton oneline"><span>Donate now</span></a>
+			
+				
+				<br/>
+			
+				<div class="panel">
+					<h3 id="twitter">Follow us on Twitter</h3>
+					<div class="tweet" id="twitter_update_list">
+						
+					</div>
+					
+					<h3 id="mail">Get our newsletter</h3>
+					<div id="mc_embed_signup">
+						<form action="http://childsifoundation.us1.list-manage.com/subscribe/post?u=b2213620d99841943c0ffc9ed&amp;id=1774e56137" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
+							<div class="clear">
+							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						</form>
+					</div>
+					
+				</div>
+				<br/>
+				
+			
+			</div>
+		
+		
+		
+		
+		</div>
+		
+	
+	</div>
+	
+	
+	
+
+	<div id="superfooter">
+		
+		<div class="container">
+			
+			<div class="row">
+				<div class="col" id="legals">
+					<p><img src="/images/cif/tag-i.png"></p>
+					<p>Child's i Foundation is a charitable company limited by guarantee, registered in England and Wales with registered company number 6674427 and registered charity number 1126212.</p>
+					<p><a href="http://www.childsifoundation.org/contact/terms-of-use-and-disclaimer/">Site Terms and Conditions</a></p>
+					<p><a href="http://www.childsifoundation.org/contact/">Contact Details</a></p>
+				</div>
+				
+				
+				<div class="col" id="quicklinks">
+					<h3>Jump to...</h3>
+					<a href="http://getinvolved.childsifoundation.org/">Get Involved</a><br/>
+					<a href="http://childsifoundation.org/give/">Donate</a><br/>
+					<a href="http://childsifoundation.org/blog/">Updates</a><br/>
+					<a href="http://twitter.com/childsi">Twitter</a> &amp; <a href="http://www.facebook.com/group.php?gid=100198310303">Facebook</a>
+				</div>
+				
+				
+				<div class="col" id="madewith">
+					<h3>Made with...</h3>
+					<a href="http://getinvolved.childsifoundation.org/supporters/">Love</a> and Wordpress.<br/>
+				</div>
+				
+				
+			</div>
+		
+		</div>
+		
+	
+	</div>
+	
+	
+	<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
+	<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/childsi.json?callback=twitterCallback2&count=1"></script>
+	
+	
+	<script type="text/javascript"> 
+
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+	</script> 
+
+	<script type="text/javascript"> 
+
+	try {
+
+	var pageTracker = _gat._getTracker("UA-8377911-1");
+
+	pageTracker._trackPageview();
+
+	} catch(err) {}</script> 
+
+	
+<!-- End Document
+================================================== -->
+</body>
+</html>
